2ee95ccca31674677736ccba6b1dff2559d2eea7
AckmanDESU <torinoob@gmail.com>
2018-10-12 17:15:22 +0200

    Solve everything

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,18 @@
 class Timer
-  #write your code here
+  attr_accessor :seconds
+
+  def initialize
+    @seconds = 0
+  end
+
+  def seconds= seconds
+    @hours = seconds / 3600
+    seconds = seconds % 3600
+    @minutes = seconds / 60
+    @seconds = seconds % 60
+  end
+
+  def time_string
+    @hours.to_s.rjust(2, '0') +":"+ @minutes.to_s.rjust(2, '0') +":"+ @seconds.to_s.rjust(2, '0')
+  end
 end
